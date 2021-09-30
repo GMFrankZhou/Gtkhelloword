6381a8663a7dbde3238de28b539917cb1797ea6b
Joe Zhou <zhoujiahong1977@gmail.com>
2021-09-29 21:28:30 -0700

    g

--- Hwwindow.cpp
+++ Hwwindow.cpp
@@ -1,10 +1,13 @@
 #include "Hwwindow.h"
 #include <iostream>
 
+
+
 Hwwindow::Hwwindow():m_button("Hello World!")
 {
     
     m_button.signal_clicked().connect(sigc::mem_fun(*this,&Hwwindow::on_button_clicked));
+    m_button.set_margin_start(200);
     add(m_button);
     m_button.show();
 }
