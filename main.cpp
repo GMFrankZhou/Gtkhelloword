f203669d3a78e946b6d9a4e6609c0169fa678f9f
Joe Zhou <zhoujiahong1977@gmail.com>
2021-09-30 10:25:46 -0700

    f

--- main.cpp
+++ main.cpp
@@ -6,7 +6,4 @@ int main(int argc, char *argv[])
     Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc,argv, "org.gtkmm.example");
     Hwwindow hww;
     return app->run(hww);
-
-
-    
 }
