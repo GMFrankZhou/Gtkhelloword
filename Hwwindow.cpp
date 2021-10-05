464d1c8252f3f12c338d5cb4b373879997ce9f90
Joe Zhou <zhoujiahong1977@gmail.com>
2021-10-04 21:13:55 -0700

    tt

--- Hwwindow.cpp
+++ Hwwindow.cpp
@@ -7,14 +7,20 @@
 Hwwindow::Hwwindow()
 {
     auto builder=Gtk::Builder::create_from_file("ui-mainwindow.glade");
-    builder->get_widget("maingrid", maingrid);
-
+    builder->get_widget("m_mainbox", m_mainbox);
     builder->get_widget("m_button_1", m_button_1);
-    m_button_1->signal_clicked().connect(sigc::mem_fun(*this, &Hwwindow::on_button_clicked));
+    builder->get_widget("m_button_2", m_button_2);
     builder->get_widget("m_entry_1", m_entry_1);
+    builder->get_widget("m_entry_2", m_entry_2);
     builder->get_widget("m_label_1", m_label_1);
-    add(*maingrid);
-    set_title("Hellowworld");
+    builder->get_widget("m_label_2", m_label_2);
+    builder->get_widget("m_listbox", m_listbox);
+    builder->get_widget("m_drawing", m_drawing);
+    m_button_1->signal_clicked().connect(sigc::mem_fun(*this, &Hwwindow::on_button_clicked));
+
+    add(*m_mainbox);
+    maximize();
+    set_title("Polygon");
 }
 
 Hwwindow::~Hwwindow()
@@ -24,5 +30,5 @@ Hwwindow::~Hwwindow()
 
 void Hwwindow::on_button_clicked()
 {
-    m_label_1->set_text(m_entry_1->get_text());
+    m_drawing->draw
 }
--- Hwwindow.h
+++ Hwwindow.h
@@ -3,7 +3,9 @@
 #include <gtkmm/button.h>
 #include <gtkmm/label.h>
 #include <gtkmm/entry.h>
-#include <gtkmm/grid.h>
+#include <gtkmm/listbox.h>
+#include <gtkmm/box.h>
+#include <gtkmm/drawingarea.h>
 #include <gtkmm/applicationwindow.h>
 
 class Hwwindow:public Gtk::ApplicationWindow
@@ -14,9 +16,14 @@ class Hwwindow:public Gtk::ApplicationWindow
     protected:
         void on_button_clicked();
         Gtk::Button *m_button_1;
+        Gtk::Button *m_button_2;
         Gtk::Label *m_label_1;
+        Gtk::Label *m_label_2;
         Gtk::Entry *m_entry_1;
-        Gtk::Grid *maingrid;
+        Gtk::Entry *m_entry_2;
+        Gtk::ListBox *m_listbox;
+        Gtk::DrawingArea *m_drawing;
+        Gtk::Box *m_mainbox;
 };
 
 #endif
