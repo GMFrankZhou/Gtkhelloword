bb7d7073bf2e253e477d4a3dbde851af023a9a38
GMFrankZhou <zhoujiahong1977@gmail.com>
2021-10-06 16:24:23 -0700

    tt

--- Hwwindow.cpp
+++ Hwwindow.cpp
@@ -1,6 +1,6 @@
 #include "Hwwindow.h"
 #include "gtkmm/builder.h"
-#include <cairomm/context.h>
+
 #include <iostream>
 
 
@@ -16,26 +16,27 @@ Hwwindow::Hwwindow()
     builder->get_widget("m_label_1", m_label_1);
     builder->get_widget("m_label_2", m_label_2);
     builder->get_widget("m_listbox", m_listbox);
-    builder->get_widget("m_draw", m_draw);
+    m_drawing = new Canvas(this);    
+    m_drawing->show();
+
     m_button_1->signal_clicked().connect(sigc::mem_fun(*this, &Hwwindow::m_button_1_on_clicked));
-    m_draw->signal_draw().connect(sigc::mem_fun(*this, &Hwwindow::m_draw_on_draw));
+
+    m_mainbox->pack_end(*m_drawing);
     add(*m_mainbox);
     set_title("Polygon");
 }
 
-int Hwwindow::cx(const int x)
-{
-    return m_draw_maxw + x;
-}
-
-int Hwwindow::cy(const int y)
-{
-    return m_draw_maxh - y;
-}
-
 Hwwindow::~Hwwindow()
 {
-
+    delete m_button_1;
+    delete m_button_2;
+    delete m_label_1;
+    delete m_label_2;
+    delete m_entry_1;
+    delete m_entry_2;
+    delete m_listbox;
+    delete m_mainbox;
+    delete m_drawing;
 }
 
 void Hwwindow::m_button_1_on_clicked()
@@ -43,19 +44,40 @@ void Hwwindow::m_button_1_on_clicked()
 
 }
 
-bool Hwwindow::m_draw_on_draw(const Cairo::RefPtr<Cairo::Context> cr)
+bool Canvas::on_draw(const Cairo::RefPtr<Cairo::Context> &cr)
 {
-    m_draw_maxh=m_draw->get_allocation().get_height()/2;
-    m_draw_maxw= (m_draw->get_allocation().get_width()-10)/2;
-    m_label_1->set_text(std::to_string(m_draw_maxh));
-    m_label_2->set_text(std::to_string(m_draw_maxw));
+    maxh=get_allocation().get_height()/2;
+    maxw=(get_allocation().get_width()-10)/2;
+    parent->get_entry_1()->set_tooltip_text(std::to_string(-maxw).append(" to ").append(std::to_string(maxw)));
+    parent->get_entry_2()->set_tooltip_text(std::to_string(-maxh).append(" to ").append(std::to_string(maxh)));
 
     cr->set_line_width(3);
     cr->set_source_rgb(0.5, 0, 0);
-    cr->move_to(cx(-m_draw_maxw), cy(0));
-    cr->line_to(cx(m_draw_maxw), cy(0));
-    cr->move_to(cx(0), cy(m_draw_maxh));
-    cr->line_to(cx(0), cy(-m_draw_maxh));
+    cr->move_to(cx(-maxw), cy(0));
+    cr->line_to(cx(maxw), cy(0));
+    cr->move_to(cx(0), cy(maxh));
+    cr->line_to(cx(0), cy(-maxh));
     cr->stroke();
     return true;
+}
+
+
+Canvas::Canvas(Hwwindow *p):parent(p)
+{
+    property_height_request() = 700;
+    property_width_request() = 700;
+}
+Canvas::~Canvas()
+{
+    delete parent;
+};
+
+int Canvas::cx(const int x)
+{
+    return maxw + x;
+}
+
+int Canvas::cy(const int y)
+{
+    return maxh - y;
 }
--- Hwwindow.h
+++ Hwwindow.h
@@ -7,18 +7,34 @@
 #include <gtkmm/box.h>
 #include <gtkmm/drawingarea.h>
 #include <gtkmm/applicationwindow.h>
-#include <cairomm/context.h>
 
-class Hwwindow:public Gtk::ApplicationWindow
+class Hwwindow;
+class Canvas : public Gtk::DrawingArea
+{
+    public:
+        Canvas(Hwwindow *p);
+        virtual ~Canvas();
+        int cx(const int);
+        int cy(const int);
+        int get_maxh() const { return maxh; }
+        int get_maxw() const { return maxw; }
+
+    private:
+        int maxh,maxw;
+        bool on_draw(const Cairo::RefPtr<Cairo::Context> &) override;
+        Hwwindow *parent;
+};
+
+class Hwwindow : public Gtk::ApplicationWindow
 {
     public:
         Hwwindow();
         virtual ~Hwwindow();
+        Gtk::Entry *get_entry_1() const { return m_entry_1; }
+        Gtk::Entry *get_entry_2() const { return m_entry_2; }
+        
     private:
         void m_button_1_on_clicked();
-        bool m_draw_on_draw(const Cairo::RefPtr<Cairo::Context> cr);
-        int cx(const int);
-        int cy(const int);
         Gtk::Button *m_button_1;
         Gtk::Button *m_button_2;
         Gtk::Label *m_label_1;
@@ -26,9 +42,8 @@ class Hwwindow:public Gtk::ApplicationWindow
         Gtk::Entry *m_entry_1;
         Gtk::Entry *m_entry_2;
         Gtk::ListBox *m_listbox;
-        Gtk::DrawingArea *m_draw;
         Gtk::Box *m_mainbox;
-        int m_draw_maxh,m_draw_maxw;
+        Canvas *m_drawing;
 };
 
 #endif
