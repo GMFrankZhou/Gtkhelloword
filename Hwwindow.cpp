ac56bb69a24c7f792c03395d161a064ef934f9ce
Joe Zhou <zhoujiahong1977@gmail.com>
2021-10-05 20:18:07 -0700

    c

--- Hwwindow.cpp
+++ Hwwindow.cpp
@@ -1,12 +1,13 @@
 #include "Hwwindow.h"
 #include "gtkmm/builder.h"
+#include <cairomm/context.h>
 #include <iostream>
-
 
 
 Hwwindow::Hwwindow()
 {
-    auto builder=Gtk::Builder::create_from_file("ui-mainwindow.glade");
+    //auto builder = Gtk::Builder::create_from_file("ui-mainwindow.glade");
+    auto builder = Gtk::Builder::create_from_resource("/unique/prefix/ui-mainwindow.glade");
     builder->get_widget("m_mainbox", m_mainbox);
     builder->get_widget("m_button_1", m_button_1);
     builder->get_widget("m_button_2", m_button_2);
@@ -15,12 +16,21 @@ Hwwindow::Hwwindow()
     builder->get_widget("m_label_1", m_label_1);
     builder->get_widget("m_label_2", m_label_2);
     builder->get_widget("m_listbox", m_listbox);
-    builder->get_widget("m_drawing", m_drawing);
-    m_button_1->signal_clicked().connect(sigc::mem_fun(*this, &Hwwindow::on_button_clicked));
-
+    builder->get_widget("m_draw", m_draw);
+    m_button_1->signal_clicked().connect(sigc::mem_fun(*this, &Hwwindow::m_button_1_on_clicked));
+    m_draw->signal_draw().connect(sigc::mem_fun(*this, &Hwwindow::m_draw_on_draw));
     add(*m_mainbox);
-    maximize();
     set_title("Polygon");
+}
+
+int Hwwindow::cx(const int x)
+{
+    return m_draw_maxw + x;
+}
+
+int Hwwindow::cy(const int y)
+{
+    return m_draw_maxh - y;
 }
 
 Hwwindow::~Hwwindow()
@@ -28,7 +38,24 @@ Hwwindow::~Hwwindow()
 
 }
 
-void Hwwindow::on_button_clicked()
+void Hwwindow::m_button_1_on_clicked()
 {
-    m_drawing->draw
+
+}
+
+bool Hwwindow::m_draw_on_draw(const Cairo::RefPtr<Cairo::Context> cr)
+{
+    m_draw_maxh=m_draw->get_allocation().get_height()/2;
+    m_draw_maxw= (m_draw->get_allocation().get_width()-10)/2;
+    m_label_1->set_text(std::to_string(m_draw_maxh));
+    m_label_2->set_text(std::to_string(m_draw_maxw));
+
+    cr->set_line_width(3);
+    cr->set_source_rgb(0.5, 0, 0);
+    cr->move_to(cx(-m_draw_maxw), cy(0));
+    cr->line_to(cx(m_draw_maxw), cy(0));
+    cr->move_to(cx(0), cy(m_draw_maxh));
+    cr->line_to(cx(0), cy(-m_draw_maxh));
+    cr->stroke();
+    return true;
 }
--- Hwwindow.h
+++ Hwwindow.h
@@ -7,14 +7,18 @@
 #include <gtkmm/box.h>
 #include <gtkmm/drawingarea.h>
 #include <gtkmm/applicationwindow.h>
+#include <cairomm/context.h>
 
 class Hwwindow:public Gtk::ApplicationWindow
 {
     public:
         Hwwindow();
         virtual ~Hwwindow();
-    protected:
-        void on_button_clicked();
+    private:
+        void m_button_1_on_clicked();
+        bool m_draw_on_draw(const Cairo::RefPtr<Cairo::Context> cr);
+        int cx(const int);
+        int cy(const int);
         Gtk::Button *m_button_1;
         Gtk::Button *m_button_2;
         Gtk::Label *m_label_1;
@@ -22,8 +26,9 @@ class Hwwindow:public Gtk::ApplicationWindow
         Gtk::Entry *m_entry_1;
         Gtk::Entry *m_entry_2;
         Gtk::ListBox *m_listbox;
-        Gtk::DrawingArea *m_drawing;
+        Gtk::DrawingArea *m_draw;
         Gtk::Box *m_mainbox;
+        int m_draw_maxh,m_draw_maxw;
 };
 
 #endif
